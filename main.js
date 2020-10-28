9a86274157965db9a0a1ed427431ec8deebfa129
Will Kuhlke <willkuhlke@Wills-MacBook-Pro-2.local>
2020-10-28 01:35:21 -0400

    gave an activation function to the product details page

--- main.js
+++ main.js
@@ -34,26 +34,61 @@ function addToCart(){
 	//looping through the array of radio buttons to find the checked slection
 	for (var i = 0; i < colors.length; i++){
 		//check if checked
-		if(colors[i].checked)
+		if(colors[i].checked){
 			color = colors[i].value;
+		}
 	}
 	var sizes = document.getElementsByName("size");
 	var size;
 	//looping through the array of radio buttons to find the checked slection... again
 	for (var i = 0; i < sizes.length; i++){
 		//check if checked
-		if(sizes[i].checked)
+		if(sizes[i].checked){
 			size = sizes[i].value;	
+		}
 	}
-console.log(productName);
-console.log(price);
-console.log(color);
-console.log(size);
-console.log(quantity);
-console.log("added to cart");
+	console.log(productName);
+	console.log(price);
+	console.log(color);
+	console.log(size);
+	console.log(quantity);
+	console.log("added to cart");
 }
+//need a function to check if all required fields are filled in to allow useres to activate the add to cart button
+function fieldsCheck(){
+	//these will be the values to change to determine if all fields are complete
+	console.log("running check");
+	var sizes = document.getElementsByName("size");
+	var sizeCheck = false;
+	var colors = document.getElementsByName("color");
+	var colorCheck = false;
+	var quantity = document.getElementById("qty").value;
+	var qntCheck = false;
 
+	for (var i = 0; i < sizes.length; i++) {
+		if(sizes[i].checked){
+			sizeCheck = true;
+			console.log("checking size");
+		}	
+	}
+	for (var i = 0; i < colors.length; i++) {
+		if(colors[i].checked){
+			colorCheck = true;
+			console.log("checking color");
+		}
+			
+	}
+	if (quantity >= 1){
+		qntCheck = true;
+		console.log("checking qty");
+	}
 
+	if (sizeCheck && colorCheck && qntCheck){
+		document.getElementById("addToCart").disabled = false;
+		console.log("size true");
+	}
+ 
+}
 
 //holy shit it works!!
 //let test = new Product("prod", "red", "large", 20, 2);
